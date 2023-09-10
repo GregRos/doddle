620359a9157cd696057e870ad5ea408cd6f13be6
GregRos <life.gregr@gmail.com>
2023-09-11 01:25:57 +0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+const path = require("path");
+module.exports = {
+    root: true,
+    extends: ["@gregros/eslint-config"],
+    parserOptions: {
+        project: [
+            path.join(__dirname, "src", "lib", "tsconfig.json"),
+            
+            path.join(__dirname, "src", "test", "tsconfig.json")
+            
+        ]
+    }
+};
