e8da8fddce2433294058751ca115822a2bd4047a
GregRos <life.gregr@gmail.com>
2024-03-22 22:02:50 +0200

    Fixed up memoize

--- src/lib/lazy/lazy.ts
+++ src/lib/lazy/lazy.ts
@@ -183,11 +183,5 @@ export function lazy<T>(initializer: () => T | Lazy<T>): Lazy<T> {
 }
 
 export function memoize<T>(definition: () => T): () => Pulled<T> {
-    let value: T;
-    return lazy(() => {
-        if (value === undefined) {
-            value = definition();
-        }
-        return value;
-    }).pull;
+    return lazy(definition).pull;
 }
