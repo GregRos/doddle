1acb8f51b7cdb6fc44e08e957ec41cbcfb351cdb
GregRos <life.gregr@gmail.com>
2024-08-06 11:59:05 +0300

    clean up eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
-const path = require("path")
-const projects =
-    /** @type {import("eslint").Linter.Config} */
-    (
-        module.exports = {
-            root: true,
-            extends: ["@gregros/eslint-config"],
-            parserOptions: {
-                project: ["./src/tsconfig.json"]
-            },
-            rules: {
-                "no-invalid-this": "off",
-                "@typescript-eslint/no-namespace": "off",
-                // Used a lot in tests as part of test cases with a title
-                "@typescript-eslint/ban-ts-comment": "off",
-                // Unused vars are needed often in tests
-                "@typescript-eslint/no-unused-vars": ["error", {
-                    "varsIgnorePattern": "^_+$",
-                    "argsIgnorePattern": "^(_+|expect)$"
-                }],
-                // lots of empty statements appear in tests
-                "no-empty": "off",
-                // Explicit control over return types
-                "@typescript-eslint/promise-function-async": "off",
-                // Explicit control over what generators do
-                "require-yield": "off",
-                // Don't mess up named functions!
-                "prefer-arrow-callback": "off",
-            }
-        }
-    )
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+    root: true,
+    extends: ["@gregros/eslint-config"],
+    parserOptions: {
+        project: ["./src/tsconfig.json"]
+    },
+    rules: {
+        "no-invalid-this": "off",
+        "@typescript-eslint/no-namespace": "off",
+        // Used a lot in tests as part of test cases with a title
+        "@typescript-eslint/ban-ts-comment": "off",
+        // Unused vars are needed often in tests
+        "@typescript-eslint/no-unused-vars": ["error", {
+            "varsIgnorePattern": "^_+$",
+            "argsIgnorePattern": "^(_+|expect)$"
+        }],
+        // lots of empty statements appear in tests
+        "no-empty": "off",
+        // Explicit control over return types
+        "@typescript-eslint/promise-function-async": "off",
+        // Explicit control over what generators do
+        "require-yield": "off",
+        // Don't mess up named functions!
+        "prefer-arrow-callback": "off",
+    }
+}
+
