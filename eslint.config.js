498e35fc3c5218e6fa0f9a2647a8983f1d98c298
GregRos <life.gregr@gmail.com>
2025-08-04 15:07:24 +0300

    chore: new linter config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,21 @@
 import preset from "@gregros/eslint-config"
-
+import markdown from "eslint-plugin-markdown"
 /** @type {import("eslint").Linter.Config[]} */
 export default [
-    ...preset
+    {
+        files: ["*.md"],
+        processor: markdown.processors.markdown
+    },
+    ...preset,
+
+    {
+        rules: {
+            // We use named functions for better stack traces
+            "prefer-arrow-callback": "off",
+            // We often need specific control over generator behavior
+            "require-yield": "off",
+            // Often need control over how functions behave
+            "ts/promise-function-async": "off"
+        }
+    }
 ]
